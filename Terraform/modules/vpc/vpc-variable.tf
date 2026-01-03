9a23c3dc3e09ff6ee92974fee648d87179f710b0
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2026-01-03 20:56:33 +0530

    Create vpc-variable.tf

---
+++ Terraform/modules/vpc/vpc-variable.tf
@@ -0,0 +1,20 @@
+variable "vpc_cidr_block" {
+    type = string
+
+}
+
+
+variable "subnet_cidr_block" {
+  type = string 
+}
+
+
+variable "az" {
+  type = string 
+  
+}
+
+variable "public_ip" {
+  type = bool
+ 
+}
